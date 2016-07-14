37466a243d1d472830c6af98aaccc40614698ee0
tanruixing <trxx@163.com>
2016-07-14 08:20:38 +0800

    测试完成

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 
-var http = require('http');
+var request = require('request');
 
-function app(url){
-	var optionspost = {  
-        host : 'data.zz.baidu.com',  
-        port : '80',  
-        path : '/urls?site=www.fe178.com&token=hzK64gK4fkBzseX2',  
-        method : 'POST'
-    };  
-
+function push(url, list, cb){
+    if (typeof list === 'string') {
+        list = [list];
+    }
     // do the POST call  
-    // 服务器端发送REST请求  
-    var reqPost = http.request(optionspost, function(resPost) {  
-        console.log(resPost)
-    });  
-
-    reqPost.end();  
-
-    reqPost.on('error', function(e) {  
-        console.error(e);  
-    });  
+    // 服务器端发送REST请求 
+    var reqPost = request(url, {
+        form: list.join('\n')
+    }, function(err, res, rep) {
+        cb(err, res, rep);
+    });   
 }
 
-module.exports = app;
+module.exports = push;
--- test.js
+++ test.js
@@ -1,2 +1,4 @@
 var img = require('./index.js');
-img('http://www.fe178.com/?p=93788');
+img(' http://data.zz.baidu.com/urls?site=www.fe178.com&token=hzK64gK4fkBzseX2','http://www.fe178.com/?p=93788', function(err, res, rep) {
+	console.log(rep)
+});
