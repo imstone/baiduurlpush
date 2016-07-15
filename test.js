b08e06b8066f678201cef2cb8ceec872b00a05ae
tanruixing <trxx@163.com>
2016-07-15 11:03:44 +0800

    完成

--- test.js
+++ test.js
@@ -1,4 +1,16 @@
 var img = require('./index.js');
-img(' http://data.zz.baidu.com/urls?site=www.fe178.com&token=hzK64gK4fkBzseX2','http://www.fe178.com/?p=93788', function(err, res, rep) {
-	console.log(rep)
-});
+// img(' http://data.zz.baidu.com/urls?site=www.fe178.com&token=hzK64gK4fkBzseX2','http://www.fe178.com/?p=95577', function(err, res, rep) {
+// 	console.log(rep)
+// });
+
+
+// }
+var i =25009
+setInterval(function(){
+	i++
+	img(' http://data.zz.baidu.com/urls?site=www.fe178.com&token=hzK64gK4fkBzseX2','www.fe178.com/?p='+i, function(err, res, rep) {
+		console.log(rep)
+		console.log(i)
+
+	});
+},20)
